3130e879382b28ffe15c7a3f0849be9ff2f1d43d
Ryan Van de Water <ryanvdw1997@berkeley.edu>
2022-03-26 10:46:42 -0700

    First part of tutorial. Set up the live server to handle asynchronous HTTP requests using routes and controllers

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,20 @@
-console.log('Hello World')
+// Backend web framework -- Express
+const express = require('express')
+// Allows to have environment variables -- dotenv
+const dotenv = require('dotenv').config()
+const port = process.env.port || 5000
+const { errorHandler } = require('./middleware/errorMiddleware')
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.use('/api/goals', require('./routes/goalRoutes')
+)
+
+app.use(errorHandler)
+
+
+
+app.listen(port, () => console.log(`Server started on port ${port}`))
